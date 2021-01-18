cdf1c4fb745b6eec6d769605b36e2ee370c0504c
Julien Poissonnier <julien@caffeine.lu>
2021-01-19 12:11:54 +1300

    bump

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts 23" />
+      <SEO title="All posts 24" />
       <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
